becd3414765b189a9735bac9682e845e5ec81872
Tung Bui <tungbuidang@gmail.com>
2024-07-21 11:40:38 +0300

    newest development

--- venice.py
+++ venice.py
@@ -12,14 +12,14 @@ pathname = "voice.wav"
 textname = "text.txt"
 
 username = 'root'
-ip = '91.199.227.82'   #change IP address accordingly 
-port = 12308          #change port accordingly 
+ip = '69.30.85.156'   #change IP address accordingly 
+port = 22049          #change port accordingly 
 key_path = '/home/tung/.ssh/id'  # path to private key 
 
 local_path = 'text.txt'
-# local_path = "runpod_watchdog.py"
 remote_path = '/workspace/Open-Sora/assets/texts/t2v.txt'
-# remote_path = 'workspace/Open-Sora/runpod_watchdog.py'
+backup_folder_path = '/home/tung/Desktop/Master/AI_Light/backup/'
+
 def ssh_client(username, ip, port, key_path):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -30,22 +30,35 @@ def send_file(client, local_path, remote_path):
     with SCPClient(client.get_transport()) as scp:
         scp.put(local_path, remote_path) 
 
-def add_style(text):
-    style1 = "Black on white, line sketching"
-    style2 = "Colorful, cartoonish"
-    style3 = "Mystic, dark"
-    final_text = text + "." + style1
-    return final_text
+# def add_style(text):
+#     style1 = "Black on white, line sketching, fish eyes"
+#     style2 = "Sand art, sand painting, fish eyes"
+#     style3 = "Minimalism, simple sketch"
+#     style4 = "In the style of Monet, monochrome"
+#     final_text = text + "." + style1 + "\n" + text + "." + style2 + "\n" + text + "." + style3 + "\n" + text + "." + style4 + "\n" 
+#     return final_text
+
+# FUNCTION4: Generate the next filename with a running number in the specified directory.
+def get_next_filename(path, prefix, extension):
+    existing_files = [f for f in os.listdir(path) if f.startswith(prefix) and f.endswith(extension)]
+    if not existing_files:
+        return f"{prefix}0000{extension}"
+    existing_files.sort()
+    last_file = existing_files[-1]
+    last_number = int(last_file[len(prefix):-len(extension)])
+    next_number = last_number + 1
+    return f"{prefix}{next_number:04d}{extension}"
+
+
+
 
 if __name__ == "__main__":
     my_sound = sound.record_microphone()
     my_text = voice_recognition.transcribe_audio_path(pathname)
-    print(my_text)
-    print(add_style(my_text))
+    backup_name = get_next_filename(backup_folder_path, 'text', '.txt')
+    file_directory = backup_folder_path + backup_name
+    f = open(file_directory, "w")
+    f.write(my_text)
 
     client = ssh_client(username, ip, port, key_path)
-
-    send_file(client, local_path, remote_path) # to be tested 
-
-    # scp = SCPClient(ssh.get_transport())
-    # scp.put(textname,destination_directory)
+    send_file(client, local_path, remote_path) 
--- voice_recognition.py
+++ voice_recognition.py
@@ -6,11 +6,20 @@ from pydub.silence import split_on_silence
 r=sr.Recognizer() 
 pathname = "voice.wav" 
 output = "text.txt"
+def add_style(text):
+    style1 = "Monochrome, high contrast, fisheye lens effect, moody, in motion"
+    style2 = "Monochrome, high contrast"
+    style3 = "black and white, lively, in motion"
+    style4 = "black and white, high contrast, rapid change, eerily"
+    style5 = "monochrome, lively, eerily, realistic"
+    final_text = text + "." + style1 + "\n" + text + "." + style2 + "\n" + text + "." + style3 + "\n" + text + "." + style4 + "\n" + text + "." +style5 + "\n" 
+    return final_text
 
 def transcribe_audio_path(path): 
     with sr.AudioFile(path) as source:
         audio_listened = r.record(source) 
         text = r.recognize_google(audio_listened)
+        text = add_style(text)
         f = open("text.txt", "w")
         f.write(text)
     return text
@@ -21,6 +30,6 @@ def transcribe_audio_path(path):
 #         text = r.recognize_google(audio_listened)
 #         return text 
     
-if __name__ == "__main__":
-    text = transcribe_audio_path(pathname)
-    print(text)  
+# if __name__ == "__main__":
+#     text = transcribe_audio_path(pathname)
+#     print(text)  
